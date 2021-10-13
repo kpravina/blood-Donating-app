f04c3c2bfb3f6ad90813eb51cec8bd9b2c570ab2
github_kpravina <github_pravinapravi15@gmail.com>
2021-10-13 14:28:15 +0530

    Application for blood donation

---
+++ app.js
@@ -0,0 +1,62 @@
+// function DonorDetails() {
+//     let name = document.querySelector('.name').value
+//     let age = document.querySelector('.age').value
+//     let weight = document.querySelector('.weight').value
+//     let Phonenum = document.querySelector('.Phonenum').value
+//     localStorage.setItem(name,value)
+//     localStorage.setItem(age,value)
+//     localStorage.setItem(weight,value)
+//     localStorage.setItem(Phonenum,value)
+
+// }
+// function recive(){
+
+// }
+
+// function fetchData(){
+//     let name = document.querySelector('.name').value
+//    console.log( document.querySelector('#newone').value) = localStorage.getItem(name)
+//     let age = document.querySelector('.age').value
+//     document.querySelector('#newone').value = localStorage.getItem(age)
+//     let name = document.querySelector('.weight').value
+//     document.querySelector('#newone').value = localStorage.getItem(weight)
+//     let age = document.querySelector('.Phonenum').value
+//     document.querySelector('#newone').value = localStorage.getItem(Phonenum)
+// }
+// function myDisplayer() {
+//     let name=document.getElementById("namebox").value;
+//   }
+// function agelimit{
+//     let myPromise = new Promise(function(myResolve, myReject)
+//     let x = 0;
+//      if(x==0)
+//      {
+//         myResolve("")        
+//     }
+//     else{
+//         myReject
+//     }
+//     myPromise.then(
+//         function(value) {myDisplayer(value);},
+//         function(error) {myDisplayer(error);}
+//       );
+// }
+//  var btn=document.querySelector("button");
+//  var title=document.querySelector("h1");
+ 
+onclick=function myfunction(){
+     userdetails();
+ }
+function userdetails(){
+    var Doing = document.getElementById('namebox').value
+    document.getElementById('newone').innerHTML=Doing;
+}
+//     localStorage.setItem(userdetails,Name)
+//     btn.show=localStorage.getItem(Name,value)
+//     console.log(show)
+// }
+// function myself(){
+//     let name=prompt("Enter your name:")
+//     localStorage.getItem(name)
+//     console.log(name)
+// }
